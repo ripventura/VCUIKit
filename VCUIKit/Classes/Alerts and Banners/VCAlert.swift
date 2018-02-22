bf56e9d60e624712054e2928256d7f4eb3072076
Vitor Cesco <vitorcesco@gmail.com>
2018-02-22 17:20:34 -0300

    Bug fix and refactor on VCAlert
    - VCAlert renamed to VCSystemAlert
    - Fixed a bug where a VCSystemAlert (actionSheet style) could crash on iPad

--- VCUIKit/Classes/Alerts and Banners/VCAlert.swift
+++ VCUIKit/Classes/Alerts and Banners/VCAlert.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-open class VCAlert {
+open class VCSystemAlert {
     /**
      Shows a system Alert.
      
@@ -18,24 +18,39 @@ open class VCAlert {
          - message: The alert message.
          - actions: The Actions (buttons) to be displayed on the alert.
          - viewController: The UIViewController presenting the alert.
+         - sourceView: Used (and required) only on iPad (actionSheet style), is used as the 'popoverPresentationController' sourceView.
      */
     static open func show(style: UIAlertControllerStyle,
                           withTitle title: String?,
                           withMessage message: String?,
                           withActions actions: [UIAlertAction]?,
-                          fromViewController viewController: UIViewController) -> Void {
-        let alertViewController = UIAlertController(title: title,
+                          fromViewController viewController: UIViewController,
+                          withSourceView sourceView: UIView?) -> Void {
+        let alertController = UIAlertController(title: title,
                                                     message: message,
                                                     preferredStyle: style)
         if let actions = actions {
             for action in actions {
-                alertViewController.addAction(action)
+                alertController.addAction(action)
             }
         }
         else {
-            alertViewController.addAction(.init(title: "Ok", style: .default, handler: nil))
+            alertController.addAction(.init(title: "Ok", style: .default, handler: nil))
+        }
+        if UIDevice.current.userInterfaceIdiom == .pad && style == .actionSheet {
+            if let sourceView = sourceView {
+                if let popoverController = alertController.popoverPresentationController {
+                    popoverController.sourceView = sourceView
+                    popoverController.sourceRect = CGRect(x: sourceView.bounds.midX,
+                                                          y: sourceView.bounds.midY,
+                                                          width: 0,
+                                                          height: 0)
+                }
+            } else {
+                fatalError("SourceView is required on all 'pad' userInterfaceIdiom.")
+            }
         }
         
-        viewController.present(alertViewController, animated: true, completion: nil)
+        viewController.present(alertController, animated: true, completion: nil)
     }
 }
